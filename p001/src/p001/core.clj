fb9ed456bc53e754ef6ac7473e69e72580bf2f94
Stefhen Hovland <stefhen.hovland@gmail.com>
2020-12-04 12:54:37 -0600

    p001

---
+++ p001/src/p001/core.clj
@@ -0,0 +1,23 @@
+(ns p001.core)
+
+;  Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+;  In your expense report, what is the product of the three entries that sum to 2020?
+
+(require '[clojure.string :as str])
+(require '[clojure.math.combinatorics :as c])
+
+(defn parse-input [i]
+  (->> i
+       (map str)
+       (map clojure.edn/read-string)))
+
+(def input (parse-input (str/split (slurp "resources/001.txt") #"\n")))
+(def pairs (c/combinations input 2))
+(def triples (c/combinations input 3))
+
+(defn sum-to-2020? [x]
+  (= 2020 (apply + x)))
+
+(defn -main [& _]
+  (println (apply * (first (filter sum-to-2020? pairs))))
+  (println (apply * (first (filter sum-to-2020? triples)))))
