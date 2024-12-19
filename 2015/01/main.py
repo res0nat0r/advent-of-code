d8fc5350d88189190be68efa133fa58a9718d9c5
Stefhen Hovland <stefhen.hovland@gmail.com>
2024-12-19 00:30:49 -0600

    wip

--- 2015/01/main.py
+++ 2015/01/main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-# Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+# Santa is trying to deliver presents in a large apartment building, but he
+# can't find the right floor - the directions he got are a little confusing. He
+# starts on the ground floor (floor 0) and then follows the instructions one
+# character at a time.
 
-# An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+# An opening parenthesis, (, means he should go up one floor, and a closing
+# parenthesis, ), means he should go down one floor.
 
-# The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
+# The apartment building is very tall, and the basement is very deep; he will
+# never find the top or bottom floors.
 
 # For example:
 
@@ -25,3 +30,9 @@ with open('data/input.txt') as f:
             floor = floor - 1
 
 print(floor)
+
+
+# Now, given the same instructions, find the position of the first character
+# that causes him to enter the basement (floor -1). The first character in the
+# instructions has position 1, the second character has position 2, and so on.
+
